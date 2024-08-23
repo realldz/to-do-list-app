31089ad569545f879f0a42a9d148b1df9167cab7
TonikakuKawaii <31581520+realldz@users.noreply.github.com>
2024-08-24 04:01:32 +0700

    feat: add some routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+namespace App\Http\Controllers;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [HomeController::class, 'index'])->name('index');
+Route::prefix('auth')->name('auth.')->group(function () {
+    Route::get('login', [AuthController::class, 'login'])->name('login');
+    Route::get('register', [AuthController::class, 'register'])->name('register');
 });
