d8595c70b1ba894dcd6e1605100cbc7c6d6f2849
TonikakuKawaii <31581520+realldz@users.noreply.github.com>
2024-08-26 03:25:47 +0700

    feat: home page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,10 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\Todo;
 class HomeController extends Controller
 {
-    public function index (Request $request) {
-        return view('home');
+    public function index(Request $request) {
+        $todos = Todo::all();
+        return view('home', [
+            'todos' => $todos,
+        ]);
     }
 }
