8f5cb94f9b953e69e4e3108e1b7f2c4678ecc438
TonikakuKawaii <31581520+realldz@users.noreply.github.com>
2024-08-26 03:25:30 +0700

    feat: add return response function

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,8 +6,16 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Response;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    public function success($message) {
+        Response::json(['success' => 'true', 'message' => $message]);
+    }
+    public function fail($message) {
+        Response::json(['success' => 'false', 'message' => $message]);
+    }
+
 }
