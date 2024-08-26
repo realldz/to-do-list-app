501a90334e6c45d7ec0ec9d6190f62735b7cef6c
TonikakuKawaii <31581520+realldz@users.noreply.github.com>
2024-08-26 18:44:56 +0700

    feat: complete update, create taskinfo form

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\TodoRequest;
 use App\Models\Todo;
+use Auth;
+use Exception;
 use Illuminate\Http\Request;
 use Response;
 
@@ -12,11 +14,43 @@ class TodoController extends Controller
     public function create() {
         return view('taskInfo');
     }
+    public function edit(Todo $task) {
+        return view('taskInfo', [
+            'task' => $task
+        ]);
+    }
     public function store(TodoRequest $request) {
-        if (Todo::create($request->validated())) {
-            return $this->success('Created task successfully');
-        } else {
-            return $this->fail($request->errors());
+        // dd($request->validated());
+        try {
+            Auth::user()->todo()->create($request->validated());
+            return $this->success('Created successfully');
+        } catch (Exception $e) {
+            return $this->fail($e->getMessage());
         }
-    } 
+    }
+    public function update(TodoRequest $request, Todo $task) {
+        try {
+            $task->update($request->validated());
+            return $this->success('Edit successfully');
+        } catch (Exception $e) {
+            return $this->fail($e->getMessage());
+        }
+    }
+
+    public function markAsDone(Todo $task) {
+        try {
+            $task->update(['status' => 1]);
+            return $this->success('Update successfully');
+        } catch (Exception $e) {
+            return $this->fail($e->getMessage());
+        }
+    }
+    public function delete(Todo $task) {
+        try {
+            $task->delete();
+            return $this->success('Delete successfully');
+        } catch (Exception $e) {
+            return $this->fail($e->getMessage());
+        }
+    }
 }
--- resources/views/taskInfo.blade.php
+++ resources/views/taskInfo.blade.php
@@ -1,68 +1,100 @@
 @extends('layouts')
-@section('title', 'Add New Task')
+@section('title')
+    @isset($task)
+        Edit
+    @else
+        Add new
+    @endisset task
+@endsection
 @section('content')
     <div class="container ">
-        <h1>Add new task</h1>
-            <div class="mb-3 row">
-                <label for="task-name" class="col-sm-2 col-form-label">Task Name</label>
-                <div class="col-sm-10">
-                    <input type="text" class="form-control" id="task_name">
+        <h1>
+            @isset($task)
+                Edit
+            @else
+                Add new
+            @endisset task
+        </h1>
+        <div class="mb-3 row">
+            <label for="task-name" class="col-sm-2 col-form-label">Task Name</label>
+            <div class="col-sm-10">
+                <input type="text" class="form-control" id="task_name">
+            </div>
+        </div>
+        <div class="mb-3 row">
+            <label for="date" class="col-sm-2 col-form-label">Date</label>
+            <div class="col-sm-10">
+                <input type="date" class="form-control" id="date">
+            </div>
+        </div>
+        <div class="mb-3 row">
+            <label for="priority" class="col-sm-2 col-form-label">Priority</label>
+            <div class="col-sm-10">
+                <select class="form-select" aria-label="Priority" name="priority" id="priority">
+                    <option value="1">Low</option>
+                    <option value="2">Medium</option>
+                    <option value="3">High</option>
+                </select>
+            </div>
+        </div>
+        <div class="mb-3 row">
+            <label for="status" class="col-sm-2 col-form-label">Status</label>
+            <div class="col-2">
+                <div class="form-check">
+                    <input class="form-check-input" type="radio" name="status" id="status-done" value="1" checked>
+                    <label class="form-check-label" for="status-done">
+                        Done
+                    </label>
                 </div>
             </div>
-            <div class="mb-3 row">
-                <label for="date" class="col-sm-2 col-form-label">Date</label>
-                <div class="col-sm-10">
-                    <input type="date" class="form-control" id="date">
+            <div class="col-2">
+                <div class="form-check">
+                    <input class="form-check-input" type="radio" name="status" id="status-undone" value="0">
+                    <label class="form-check-label" for="status-undone">
+                        Undone
+                    </label>
                 </div>
             </div>
-            <div class="mb-3 row">
-                <label for="priority" class="col-sm-2 col-form-label">Priority</label>
-                <div class="col-sm-10">
-                    <select class="form-select" aria-label="Priority">
-                        <option value="1">Low</option>
-                        <option value="2">Medium</option>
-                        <option value="3">High</option>
-                    </select>
-                </div>
-            </div>
-            <div class="mb-3 row">
-                <label for="status" class="col-sm-2 col-form-label">Status</label>
-                <div class="col-2">
-                    <div class="form-check">
-                        <input class="form-check-input" type="radio" name="status" id="status-done" value="1" checked>
-                        <label class="form-check-label" for="status-done">
-                            Done
-                        </label>
-                    </div>
-                </div>
-                <div class="col-2">
-                    <div class="form-check">
-                        <input class="form-check-input" type="radio" name="status" id="status-undone" value="0">
-                        <label class="form-check-label" for="status-undone">
-                            Undone
-                        </label>
-                    </div>
-                </div>
-            </div>
-            <div class="mx-0 row">
-                <button id="save_btn" type="button" class="btn btn-primary">Save</button>
-            </div>
+        </div>
+        <div class="mx-0 row">
+            <button id="save_btn" type="button" class="btn btn-primary">Save</button>
+        </div>
     </div>
     <script>
         $("#save_btn").click(function() {
             console.log('click');
             $.ajax({
-                method: 'POST',
-                url: '{{ route('task.store') }}',
+                method: @isset($task) 'PUT' @else 'POST' @endisset,
+                url: @isset($task) '{{ route('task.update',$task->id) }}' @else '{{ route('task.store') }}' @endisset,
                 dateType: 'json',
                 data: {
                     _token: '{{ csrf_token() }}',
                     task_name: $("#task_name").val(),
                     date: $("#date").val(),
                     priority: $("#priority").val(),
-                    status: $('input[name=status]:checked').val()
-                }
+                    status: $('input[name=status]:checked').val(),
+                },
+                success: function(result) {
+                    Swal.fire({
+                        text: result.message,
+                        icon: "success"
+                    }).then(() => window.location.reload());
+                },
+                error: function(result) {
+                    Swal.fire({
+                        text: result.responseJSON.message,
+                        icon: "error"
+                    }).then(() => window.location.reload());
+                },
             });
+        });
+        $(document).ready(function() {
+            @isset($task)
+                $('#task_name').val('{{ $task->task_name }}');
+                $('#date').val('{{ $task->date }}');
+                $('#priority').val({{ $task->getAttributes()['priority'] }});
+                $("input[name='status'][value='{{ $task->status }}']").click();
+            @endisset
         });
     </script>
 @endsection
