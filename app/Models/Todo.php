8b1f2f83fd08eea4e9b5a0a1db323a70751578c6
TonikakuKawaii <31581520+realldz@users.noreply.github.com>
2024-08-26 15:32:56 +0700

    fix: get priority

--- app/Models/Todo.php
+++ app/Models/Todo.php
@@ -8,12 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Todo extends Model
 {
     use HasFactory;
-    protected $fillable = ['task_name', 'status', 'date'];
+    protected $fillable = ['task_name', 'status', 'date', 'priority'];
     public function user() {
         return $this->belongsTo(User::class);
     }
     public function getPriorityAttribute() {
-        switch ($this->priority) {
+        switch ($this->attributes['priority']) {
             case 1:
                 return 'Low';
             case 2:
