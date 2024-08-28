3202215f7bbb54ac23f95fbb60eb7a6fe6c550e1
TonikakuKawaii <31581520+realldz@users.noreply.github.com>
2024-08-29 01:26:48 +0700

    chore: add logout button

--- resources/views/admin/components/sidebar.blade.php
+++ resources/views/admin/components/sidebar.blade.php
@@ -56,6 +56,14 @@
                         </p>
                     </a>
                 </li>
+                <li class="nav-item">
+                    <a href="{{ route('auth.logout') }}" class="nav-link">
+                        <i class="nav-icon fas fa-sign-out-alt"></i>
+                        <p>
+                            Logout
+                        </p>
+                    </a>
+                </li>
 
             </ul>
         </nav>
